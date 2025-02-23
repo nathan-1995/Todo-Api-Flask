1147db3d42bc325a438a92a8201a2dd09d0caf33
Nathan <ndelima1@gmail.com>
2025-02-23 16:31:29 +0530

    added type hints

--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone  
+from typing import Optional
 
 # Get the current time in UTC
-def utc_now():
+def utc_now() -> datetime:
     return datetime.now(timezone.utc)
 
-db = SQLAlchemy()
+db: SQLAlchemy = SQLAlchemy()
 
 class Task(db.Model):
     __tablename__ = "task"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(255))
-    completed = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime(timezone=True), default=utc_now)
-    updated_at = db.Column(db.DateTime(timezone=True), default=utc_now, onupdate=utc_now)
+    id: int = db.Column(db.Integer, primary_key=True)
+    title: str = db.Column(db.String(100), nullable=False)
+    description: Optional[str] = db.Column(db.String(255))
+    completed: bool = db.Column(db.Boolean, default=False)
+    created_at: datetime = db.Column(db.DateTime(timezone=True), default=utc_now)
+    updated_at: datetime = db.Column(db.DateTime(timezone=True), default=utc_now, onupdate=utc_now)
